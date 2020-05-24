ce840b81d9f287b99df23467d60a070af4986721
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-24 22:21:39 +0200

    ADD toolbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-import "./App.css";
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Tollbar from '@material-ui/core/Toolbar';
 import LocationList from "./components/LocationList";
 import { Grid, Col, Row } from "react-flexbox-grid";
+import "./App.css";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const cities = [
   "Barcelona, es",
@@ -19,7 +24,15 @@ class App extends Component {
     return (
       <div className="App">
         <Grid>
-          <Row>Navigation Bar</Row>
+          <Row>
+            <AppBar position='sticky'>
+              <Toolbar>
+                <Typography variant='title' color='inherit'>
+                    Weather App
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Row>
           <Row>
             <Col xs={12} md={6}>
               <LocationList
