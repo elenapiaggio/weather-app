71dd2e3023d2c6d64165fee829cbd8a09bc9c5ee
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-24 13:31:33 +0200

    ADD LocationList component

---
+++ src/components/LocationList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherLocation from "./WeatherLocation/Index";
+
+const LocationList = () => {
+  return (
+    <div>
+      <WeatherLocation city="Bogota, es" />
+    </div>
+  );
+};
+
+export default LocationList;
