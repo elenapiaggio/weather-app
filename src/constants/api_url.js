d3b41112f553cf9d87815bdbfd46355fa5b239ad
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-24 11:21:34 +0200

    UPDATE city

--- src/constants/api_url.js
+++ src/constants/api_url.js
@@ -1,4 +1,4 @@
-const location = "Buenos Aires";
+const location = "Barcelona";
 
 const api_key = "cee0c281dcb7298c5e4cb87bc06dca36";
 const url_base_weather = "https://api.openweathermap.org/data/2.5/weather";
